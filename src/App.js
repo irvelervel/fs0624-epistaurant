2c2a54f18d20f2961db790e0a1881ac28b2e470d
Stefano Casasola <ste.casasola@gmail.com>
2024-08-20 10:56:33 +0200

    Adds custom navbar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-import logo from './logo.svg'
 // RICORDIAMOCI DI INCLUDERE IN APP.JS IL FILE CSS DI BOOTSTRAP PRESENTE IN NODE_MODULES
 // !!!
 import 'bootstrap/dist/css/bootstrap.min.css'
 // !!!
 import './App.css'
+import CustomNavbar from './components/CustomNavbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* ora siete liberi di utilizzare le classi di bootstrap */}
-        <p className="text-success mt-5 border border-danger">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <CustomNavbar />
       </header>
-    </div>
+      <main>
+        <h2 className="text-center mt-5">IL CAROSELLO ANDRÀ QUA</h2>
+      </main>
+    </>
   )
 }
 
